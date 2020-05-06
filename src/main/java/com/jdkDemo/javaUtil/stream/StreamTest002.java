5c4fa21fb510eab05349dbaa2150995ebf26e512
xiaoerque <xiaoerque@126.com>
2020-05-06 19:44:57 +0800

    intStream的方法:summaryStatistics

--- src/main/java/com/jdkDemo/javaUtil/stream/StreamTest002.java
+++ src/main/java/com/jdkDemo/javaUtil/stream/StreamTest002.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * 测试关于管道的内容
@@ -19,10 +21,19 @@ public class StreamTest002 {
 
         long count = listArrays.stream().filter(dog -> dog.getAge() > 1).count();
 
-        listArrays.forEach(System.out::println);
+        listArrays.stream().filter(dog -> dog.getAge() >1).forEach(System.out::println);
         System.out.println(count);
-    }
 
+        System.out.println("------下面写一个富含多个求值的方法---------");
+        Stream<Integer> integerStream = Stream.iterate(1,item-> item + 1).limit(6);
+        IntSummaryStatistics intSummaryStatistics = integerStream.filter(intStream->intStream>3).mapToInt(intStream->intStream*2).skip(1).limit(2).summaryStatistics();
+        System.out.println(intSummaryStatistics.getMax());
+        System.out.println(intSummaryStatistics.getMin());
+        System.out.println(intSummaryStatistics.getAverage());
+        System.out.println(intSummaryStatistics.getSum());
+        System.out.println(intSummaryStatistics.getCount());
+        System.out.println(intSummaryStatistics.getClass());
+    }
     @Data
     public static class Dog{
         private String name;
